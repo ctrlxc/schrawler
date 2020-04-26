38faf9a790ec5c1fd2d23f1edc0e9dd8b4522bb1
ctrlxc <ctrlpfnb@gmail.com>
2020-04-26 23:46:55 +0900

    sample poppeteer

---
+++ index.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+//   const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 250 // slow down by 250ms
+  });
+ 
+  const page = await browser.newPage();
+
+  await page.goto('http://swa.city-osaka.ed.jp/swas/index.php?id=e531066', {
+    waitUntil: 'domcontentloaded'
+  });
+
+  await page.screenshot({path: 'screenshot.png'});
+
+  await browser.close();
+})();
